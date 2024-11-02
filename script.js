aa3569c0923dc81bf2a54f822cee0c5d25e6fda6
GitaraGames <130596054+GitaraGames@users.noreply.github.com>
2024-11-02 10:11:32 +0300

    Add "Hello, User" text

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 var count = 0;
 const txt = document.getElementById("count_text");
+const hello_user = document.getElementById("hello_user");
 let tg = window.Telegram.WebApp;
+
+tg.expand();
+
+hello_user.innerText = "Hello, " + tg.initDataUnsafe.user.first_name;
 
 function click_count(){
     count++;
